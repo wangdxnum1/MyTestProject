ac478d0b487b2a5d11acf6912589597ad55e620d
wang_wonderful <wang_wonderful@foxmail.com>
2017-04-28 18:24:27 +0800

    防BOSS直聘下拉刷新

--- TWBossZhiPingRefreshHeader/TWBossZhiPingRefreshHeader/TWBossZhiPingRefreshHeader.h
+++ TWBossZhiPingRefreshHeader/TWBossZhiPingRefreshHeader/TWBossZhiPingRefreshHeader.h
@@ -8,6 +8,12 @@
 
 #import <MJRefresh/MJRefresh.h>
 
-@interface TWBossZhiPingRefreshHeader : MJRefreshGifHeader
+@interface TWBossZhiPingRefreshHeader : MJRefreshStateHeader
+
+@property (weak, nonatomic, readonly) UIImageView *gifView;
+
+/** 设置state状态下的动画图片images 动画持续时间duration*/
+- (void)setImages:(NSArray *)images duration:(NSTimeInterval)duration forState:(MJRefreshState)state;
+- (void)setImages:(NSArray *)images forState:(MJRefreshState)state;
 
 @end
--- TWBossZhiPingRefreshHeader/TWBossZhiPingRefreshHeader/TWBossZhiPingRefreshHeader.m
+++ TWBossZhiPingRefreshHeader/TWBossZhiPingRefreshHeader/TWBossZhiPingRefreshHeader.m
@@ -8,13 +8,67 @@
 
 #import "TWBossZhiPingRefreshHeader.h"
 
+@interface TWBossZhiPingRefreshHeader()
+{
+    __unsafe_unretained UIImageView *_gifView;
+}
+/** 所有状态对应的动画图片 */
+@property (strong, nonatomic) NSMutableDictionary *stateImages;
+/** 所有状态对应的动画时间 */
+@property (strong, nonatomic) NSMutableDictionary *stateDurations;
+@end
+
 @implementation TWBossZhiPingRefreshHeader
 
-#pragma mark - 重写方法
-#pragma mark 基本设置
+#pragma mark - 懒加载
+- (UIImageView *)gifView
+{
+    if (!_gifView) {
+        UIImageView *gifView = [[UIImageView alloc] init];
+        [self addSubview:_gifView = gifView];
+    }
+    return _gifView;
+}
+
+- (NSMutableDictionary *)stateImages
+{
+    if (!_stateImages) {
+        self.stateImages = [NSMutableDictionary dictionary];
+    }
+    return _stateImages;
+}
+
+- (NSMutableDictionary *)stateDurations
+{
+    if (!_stateDurations) {
+        self.stateDurations = [NSMutableDictionary dictionary];
+    }
+    return _stateDurations;
+}
+
+#pragma mark - 公共方法
+- (void)setImages:(NSArray *)images duration:(NSTimeInterval)duration forState:(MJRefreshState)state
+{
+    if (images == nil) return;
+    
+    self.stateImages[@(state)] = images;
+    self.stateDurations[@(state)] = @(duration);
+    
+    self.mj_h = 75;
+}
+
+- (void)setImages:(NSArray *)images forState:(MJRefreshState)state
+{
+    [self setImages:images duration:images.count * 0.1 forState:state];
+}
+
+#pragma mark - 实现父类的方法
 - (void)prepare
 {
     [super prepare];
+    
+    // 初始化间距
+    self.labelLeftInset = 20;
     
     // 设置普通状态的动画图片
     NSMutableArray *idleImages = [NSMutableArray array];
@@ -23,6 +77,7 @@
         [idleImages addObject:image];
     }
     [self setImages:idleImages forState:MJRefreshStateIdle];
+    [self setImages:idleImages forState:MJRefreshStatePulling];
     
     // 设置即将刷新状态的动画图片（一松开就会刷新的状态）
     NSMutableArray *refreshingImages = [NSMutableArray array];
@@ -30,8 +85,6 @@
         UIImage *image = [UIImage imageNamed:[NSString stringWithFormat:@"%@r", @(i)]];
         [refreshingImages addObject:image];
     }
-
-    [self setImages:refreshingImages forState:MJRefreshStatePulling];
     
     // 设置正在刷新状态的动画图片
     [self setImages:refreshingImages forState:MJRefreshStateRefreshing];
@@ -43,8 +96,25 @@
     self.stateLabel.hidden = YES;
 }
 
-- (void)placeSubviews{
+- (void)setPullingPercent:(CGFloat)pullingPercent
+{
+    [super setPullingPercent:pullingPercent];
+    NSArray *images = self.stateImages[@(MJRefreshStateIdle)];
+    if ((self.state != MJRefreshStateIdle && self.state != MJRefreshStatePulling)|| images.count == 0) return;
+    // 停止动画
+    [self.gifView stopAnimating];
+    // 设置当前需要显示的图片
+    NSUInteger index =  images.count * pullingPercent;
+    if (index >= images.count) index = images.count - 1;
+    self.gifView.image = images[index];
+}
+
+- (void)placeSubviews
+{
     [super placeSubviews];
+    
+    if (self.gifView.constraints.count) return;
+    
     
     /* 根据图片设置控件的高度 */
     self.gifView.mj_h = 120 *0.5;
@@ -55,14 +125,26 @@
     self.gifView.contentMode = UIViewContentModeScaleToFill;
 }
 
-- (void)setImages:(NSArray *)images duration:(NSTimeInterval)duration forState:(MJRefreshState)state
+- (void)setState:(MJRefreshState)state
 {
-    [super setImages:images duration:duration forState:state];
+    MJRefreshCheckState
     
-    
-    
-    self.mj_h = 75;
-    
+    // 根据状态做事情
+    if (state == MJRefreshStateRefreshing) {
+        NSArray *images = self.stateImages[@(state)];
+        if (images.count == 0) return;
+        
+        [self.gifView stopAnimating];
+        if (images.count == 1) { // 单张图片
+            self.gifView.image = [images lastObject];
+        } else { // 多张图片
+            self.gifView.animationImages = images;
+            self.gifView.animationDuration = [self.stateDurations[@(state)] doubleValue];
+            [self.gifView startAnimating];
+        }
+    } else if (state == MJRefreshStateIdle) {
+        [self.gifView stopAnimating];
+    }
 }
 
 @end
